f2b334e990ca7ac3dc1faf642324ea3b04f36b49
cabraldasilvac <wcabraldasilvac@gmail.com>
2023-04-04 12:09:01 -0300

    :rocket: feat: backend start

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TodoList.css";
+import Icone from "./assets/icon.webp";
 
 function TodoList() {
+  const [lista, setLista] = useState([]);
+  const [novoItem, setNovoItem] = useState([]);
+
+  function adicionaItem(form) {
+    form.preventDefault();
+    if (!novoItem) {
+      return;
+    }
+    setLista([...lista, { text: novoItem, isCompleted: false }]);
+    setNovoItem("");
+    document.getElementById("input-entrada").focus();
+  }
+
+  function clicou(index) {
+    const listaAux = [...lista];
+    listaAux[index].isCompleted = !listaAux[index].isCompleted;
+    setLista(listaAux);
+  }
+
+  function deleta(index) {
+    const listaAux = [...lista];
+    listaAux.splice(index, 1);
+    setLista(listaAux);
+  }
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <form>
-        <input type="text" placeholder="Adicione uma tarefa" />
+      <form onSubmit={adicionaItem}>
+        <input
+          id="input-entrada"
+          type="text"
+          value={novoItem}
+          onChange={(e) => {
+            setNovoItem(e.target.value);
+          }}
+          placeholder="Adicione uma tarefa"
+        />
 
         <button className="add" type="submit">
           {" "}
@@ -14,15 +47,35 @@ function TodoList() {
         </button>
       </form>
       <div className="ListaTarefas">
-        <div className="item">
-          <span>Tarefas de exemplo</span>
-          <button className="del">Deletar</button>
+        <div style={{ textAlign: "center" }}>
+          {lista.length < 1 ? (
+            <img className="icone-central" src={Icone} />
+          ) : (
+            lista.map((item, index) => (
+              <div
+                key={index}
+                className={item.isCompleted ? "item completo" : "item"}
+              >
+                <span
+                  onClick={() => {
+                    clicou(index);
+                  }}
+                >
+                  {item.text}
+                </span>
+                <button
+                  onClick={() => {
+                    deleta(index);
+                  }}
+                  className="del"
+                >
+                  Deletar
+                </button>
+              </div>
+            ))
+          )}
+          <button className="deleteAll">Deletar Todas</button>
         </div>
-        <div className="item completo">
-          <span>Tarefas de exemplo</span>
-          <button className="del">Deletar</button>
-        </div>
-        <button className="deleteAll">Deletar Todas</button>
       </div>
     </div>
   );
